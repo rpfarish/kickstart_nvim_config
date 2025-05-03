9175d9ed775e31aeb7894d79bdc4f71f9631d6c9
rpfarish <rpfarish@live.com>
2025-05-03 13:40:01 -0400

    removed multiple duplicate entries in for zen-mode

--- lua/rpfarish/plugins/zen-mode.lua
+++ lua/rpfarish/plugins/zen-mode.lua
@@ -1,117 +1,18 @@
-return { 
-    'folke/zen-mode.nvim',
-    config = function()
-      -- vim.keymap.set('n', '<leader>zz', function()
-      --   require('zen-mode').setup {
-      --     window = {
-      --       width = 90,
-      --       options = {},
-      --     },
-      --   }
-      --   require('zen-mode').toggle()
-      --   vim.wo.wrap = false
-      --   vim.wo.number = true
-      --   vim.wo.rnu = true
-      -- end)
-
-      vim.keymap.set('n', '<leader>zz', function() -- was leader zZ
-        require('zen-mode').setup {
-          window = {
-            width = 180,
-            options = {},
-          },
-        }
-        require('zen-mode').toggle()
-        vim.wo.wrap = false
-        vim.wo.number = false
-        vim.wo.rnu = false
-        vim.opt.colorcolumn = '0'
-      end)
-    end,
-    config = function()
-      -- vim.keymap.set('n', '<leader>zz', function()
-      --   require('zen-mode').setup {
-      --     window = {
-      --       width = 90,
-      --       options = {},
-      --     },
-      --   }
-      --   require('zen-mode').toggle()
-      --   vim.wo.wrap = false
-      --   vim.wo.number = true
-      --   vim.wo.rnu = true
-      -- end)
-
-      vim.keymap.set('n', '<leader>zz', function() -- was leader zZ
-        require('zen-mode').setup {
-          window = {
-            width = 180,
-            options = {},
-          },
-        }
-        require('zen-mode').toggle()
-        vim.wo.wrap = false
-        vim.wo.number = false
-        vim.wo.rnu = false
-        vim.opt.colorcolumn = '0'
-      end)
-    end,
+return {
   'folke/zen-mode.nvim',
   config = function()
-    -- vim.keymap.set('n', '<leader>zz', function()
-      --   require('zen-mode').setup {
-        --     window = {
-          --       width = 90,
-          --       options = {},
-          --     },
-          --   }
-          --   require('zen-mode').toggle()
-          --   vim.wo.wrap = false
-          --   vim.wo.number = true
-          --   vim.wo.rnu = true
-          -- end)
-
-          vim.keymap.set('n', '<leader>zz', function() -- was leader zZ
-            require('zen-mode').setup {
-              window = {
-                width = 180,
-                options = {},
-              },
-            }
-            require('zen-mode').toggle()
-            vim.wo.wrap = false
-            vim.wo.number = false
-            vim.wo.rnu = false
-            vim.opt.colorcolumn = '0'
-          end)
-        end,
-        'folke/zen-mode.nvim',
-        config = function()
-          -- vim.keymap.set('n', '<leader>zz', function()
-            --   require('zen-mode').setup {
-              --     window = {
-                --       width = 90,
-                --       options = {},
-                --     },
-                --   }
-                --   require('zen-mode').toggle()
-                --   vim.wo.wrap = false
-                --   vim.wo.number = true
-                --   vim.wo.rnu = true
-                -- end)
-
-                vim.keymap.set('n', '<leader>zz', function() -- was leader zZ
-                  require('zen-mode').setup {
-                    window = {
-                      width = 180,
-                      options = {},
-                    },
-                  }
-                  require('zen-mode').toggle()
-                  vim.wo.wrap = false
-                  vim.wo.number = false
-                  vim.wo.rnu = false
-                  vim.opt.colorcolumn = '0'
-                end)
-              end,
-            }
+    vim.keymap.set('n', '<leader>zz', function() -- was leader zZ
+      require('zen-mode').setup {
+        window = {
+          width = 180,
+          options = {},
+        },
+      }
+      require('zen-mode').toggle()
+      vim.wo.wrap = false
+      vim.wo.number = false
+      vim.wo.rnu = false
+      vim.opt.colorcolumn = '0'
+    end)
+  end,
+}
